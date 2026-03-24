f64bfed788d2fec98573d55c7bd07d0d4725c3bc
AlexanderForster1 <alefor0111@gmail.com>
2026-03-24 22:19:42 +1100

    Changed node list to used dictionary

--- Assignment 2A/main.py
+++ Assignment 2A/main.py
@@ -12,33 +12,40 @@ file_path = 'PathFinder-test.txt'
 categories = ['Nodes:', 'Edges:', 'Origin:', 'Destinations:']
 destination = []
 origin = []
-node_list = []
+node_list = {}
 current_category = ''
 with open(file_path, 'r') as file:
     for line in file:
         line = line.strip()
+        
         if line in categories:
             current_category = line
-        if line != '':
+            continue
         
-            if current_category == 'Nodes:' and line != 'Nodes:':
-                node_number = int(line.split(':')[0])
-                node_coord = line.split(':')[1].replace('(', '').replace(')', '').split(',')
-                node_coord = (int(node_coord[0]), int(node_coord[1]))
-                node = Node(node_number, node_coord)
-                node_list.insert(node_number, node)
-            elif current_category == 'Edges:' and line != 'Edges:':
-                edge_info = line.split(':')
-                cost = int(edge_info[1].strip())
-                nodes = edge_info[0].strip().replace('(', '').replace(')', '').split(',')
-                nodes = (int(nodes[0]), int(nodes[1]))
-                node_list[nodes[0] - 1].add_edge(nodes[1], cost)
-            elif current_category == 'Origin:' and line != 'Origin:':
-                origin = int(line.strip())
-            elif current_category == 'Destinations:' and line != 'Destinations:':
-                destination_nodes = line.strip().split(';')
-                destination = [int(destination_node.strip()) for destination_node in destination_nodes]
-
+        
+        if line == '':
+            continue
+        
+        if current_category == 'Nodes:':
+            node_number = int(line.split(':')[0])
+            node_coord = line.split(':')[1].replace('(', '').replace(')', '').split(',')
+            node_coord = (int(node_coord[0]), int(node_coord[1]))
+            node = Node(node_number, node_coord)
+            node_list[node_number] = node
+        
+        elif current_category == 'Edges:':
+            edge_info = line.split(':')
+            cost = int(edge_info[1].strip())
+            nodes = edge_info[0].strip().replace('(', '').replace(')', '').split(',')
+            nodes = (int(nodes[0]), int(nodes[1]))
+            node_list[nodes[0]].add_edge(nodes[1], cost)
+        
+        elif current_category == 'Origin:':
+            origin = int(line.strip())
+        
+        elif current_category == 'Destinations:':
+            destination_nodes = line.strip().split(';')
+            destination = [int(destination_node.strip()) for destination_node in destination_nodes]
 
 
 
