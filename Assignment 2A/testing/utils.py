49b2f79b930517f9053a0c56c9487893570eb7ff
Demoncatco <re873nad@gmail.com>
2026-04-17 15:31:46 +1000

    implement A* search with euclidean distance heuristic,  with test cases

--- Assignment 2A/testing/utils.py
+++ Assignment 2A/testing/utils.py
@@ -6,7 +6,7 @@ import json
 import networkx as nx
 import random
 
-sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 from input import Node
 from astar import astar
 
