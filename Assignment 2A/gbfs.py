d4083b07a4be7b30144eb0c29e5f05609b79002d
AlexanderForster1 <alefor0111@gmail.com>
2026-04-17 19:08:45 +1000

    fixed problem with invalid path being generated

--- Assignment 2A/gbfs.py
+++ Assignment 2A/gbfs.py
@@ -1,20 +1,31 @@
 import heuristics
-def gbfs(node_list, destination, origin):
+def gbfs(node_list, origin, destination):
     Node_heuristic = []
     Node_heuristic.append((origin, 0))
     Node_count = 0
-    visited = []
+    visited = set()
+    parent = {origin: None}
+    goal = destination[0] if isinstance(destination, list) else destination
+
     while Node_heuristic:
         Node_heuristic.sort(key=lambda x: x[1])
         current_node = Node_heuristic.pop(0)[0]
-        Node_count += 1
         if current_node in visited:
             continue
-        visited.append(current_node)
-        if current_node in destination:
-            return current_node,  Node_count, visited
+        visited.add(current_node)
+        Node_count += 1
+
+
+        if current_node == goal:
+            path = []
+            while current_node is not None:
+                path.append(current_node)
+                current_node = parent[current_node]
+                path.reverse()
+            return goal,  Node_count, path
         for neighbor, cost in node_list[current_node].edges:
-            if neighbor not in visited:
-                heuristic_cost = heuristics.euclidean_distance(node_list[neighbor], node_list[destination[0]])
+            if neighbor not in visited and neighbor not in parent:
+                heuristic_cost = heuristics.manhattan_distance(node_list[neighbor], node_list[goal])
                 Node_heuristic.append((neighbor, heuristic_cost))
+                parent[neighbor] = current_node
     return  None, Node_count, []
