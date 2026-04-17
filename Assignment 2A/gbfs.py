581376ff058e82527994e4640fe387e9fd404a40
AlexanderForster1 <alefor0111@gmail.com>
2026-04-17 22:09:46 +1000

    Fixed issue where path wasn't being properly displayed

--- Assignment 2A/gbfs.py
+++ Assignment 2A/gbfs.py
@@ -10,10 +10,10 @@ def gbfs(node_list, origin, destination):
     while Node_heuristic:
         Node_heuristic.sort(key=lambda x: x[1])
         current_node = Node_heuristic.pop(0)[0]
+        Node_count += 1
         if current_node in visited:
             continue
         visited.add(current_node)
-        Node_count += 1
 
 
         if current_node == goal:
@@ -21,7 +21,7 @@ def gbfs(node_list, origin, destination):
             while current_node is not None:
                 path.append(current_node)
                 current_node = parent[current_node]
-                path.reverse()
+            path.reverse()
             return goal,  Node_count, path
         for neighbor, cost in node_list[current_node].edges:
             if neighbor not in visited and neighbor not in parent:
