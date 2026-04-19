eceefc2746d69c07e7caf38cd3b4b2c98728b5e6
104887896 <104887896@student.swin.edu.au>
2026-04-19 15:11:40 +1000

    removed outdated code segments from BFS

--- Assignment 2A/bfs.py
+++ Assignment 2A/bfs.py
@@ -16,17 +16,14 @@ def bfs(node_list, origin, destination):
     while queue:
         path = queue.popleft()
         sequence = path[-1]
-        #print(sequence, end = " ")
         nodes_visited += 1
 
         if sequence in destination:
-            #print(f"\nDestination found! Path length: {nodes_visited}")
             return sequence, nodes_visited, path
 
         for neighbour, cost in node_list[sequence].edges:
             if neighbour not in visited:
                 visited.add(neighbour)
-                #queue.append(neighbour)
                 queue.append(path + [neighbour])
 
     return None, nodes_visited, []
