01e2a3433abe0f0423c2fc3d9385bc1139bde448
mungkeumeomeo <nhido.1911@gmail.com>
2026-05-10 17:52:36 +1000

    some runs

--- Assignment 2B/gru.py
+++ Assignment 2B/gru.py
@@ -13,30 +13,15 @@ from datetime import datetime
 from dataclasses import dataclass, asdict
 from typing import List
 
-'''
-NOTES
-
-normalizing data for the RNNs
-https://arxiv.org/abs/1510.01378
-
-would incorporate TimeSeriesSplit but current model already encodes time structure so it's prob not that necessary and there are a bunch of data leakage issues i don't wanna think about
-
-- early stopping?
-- use exact location?
-'''
-
-PARENT_DIR = Path(__file__).resolve().parent
-TEST_SIZE  = 0.2
-features = [
-  "time_sin",
-  "time_cos",
-  "day_sin",
-  "day_cos",
-  "is_weekend",
-  "lat_scaled",
-  "lon_scaled",
-  "traffic_volume"
-]
+@dataclass
+class Config:
+  gru_layers: List[int]
+  time_step: int = 96
+  loss: str = "mean_squared_error"
+  lr: float = 0.001
+  batch_size: int = 32
+  epochs: int = 20
+  dropout: float= 0.2
 
 def create_dataset(df: pd.DataFrame, time_step: int):
   '''Create sliding windows of time_step length'''
@@ -44,14 +29,13 @@ def create_dataset(df: pd.DataFrame, time_step: int):
   input  = df[features].values
   output = df["traffic_volume"].values
   for i in range(len(df.values) - time_step):
-    # Exclude previous traffic flow from input
     X.append(input[i:(i + time_step)])
     y.append(output[i + time_step])
   return np.array(X), np.array(y)
 
-def load_data(df: pd.DataFrame, time_step=1):
+def load_data(df: pd.DataFrame, features: list[int], time_step=1, test_size=0.2):
   '''Load, scale, and split data'''
-  train_size = int(len(df) * (1 - TEST_SIZE))
+  train_size = int(len(df) * (1 - test_size))
   train_df = df[:train_size].copy()
   test_df  = df[train_size:].copy()
 
@@ -66,11 +50,10 @@ def load_data(df: pd.DataFrame, time_step=1):
   # y.shape = (samples, )
   X_train, y_train = create_dataset(train_df, time_step)
   X_test, y_test   = create_dataset(test_df, time_step)
-  print(y_train[0])
 
   return X_train, y_train, X_test, y_test, scaler
 
-def build_model(config):
+def build_model(config: Config):
   '''Build the GRU'''
   model = Sequential()
 
@@ -94,7 +77,7 @@ def build_model(config):
 
 def run(config, X_train, y_train, X_test, y_test, scaler):
   run_id  = datetime.now().strftime("%Y%m%d%H%M%S")
-  run_dir = PARENT_DIR / "runs"
+  run_dir = parent_dir / "runs"
 
   model   = build_model(config)
   history = model.fit(X_train, y_train,
@@ -111,22 +94,40 @@ def run(config, X_train, y_train, X_test, y_test, scaler):
   rmse = np.sqrt(mean_squared_error(y_test_inversed, predictions_inversed))
   mae  = mean_absolute_error(y_test_inversed, predictions_inversed)
 
-  # Save plot
-  plt.plot(y_test_inversed[:200], label="Actual")
-  plt.plot(predictions_inversed[:200], label="Predicted")
-  plt.legend()
-  plt.title(f"Run {run_id}")
-  plt.savefig(run_dir / f"preds_{run_id}.png")
-  plt.close()
+  # Save plots
+  # Save prediction plot
+  step = 50
+
+  y_true = y_test_inversed[::step]
+  y_pred = predictions_inversed[::step]
+
+  x = np.arange(0, len(y_test_inversed), step)
+
+  fig, ax = plt.subplots()
+
+  ax.plot(x[:len(y_true)], y_true, label="Actual")
+  ax.plot(x[:len(y_pred)], y_pred, label="Predicted")
+
+  ax.set_title(f"Run {run_id}")
+  ax.set_xlabel("Time Step")
+  ax.set_ylabel("Traffic Volume")
+  ax.legend()
+
+  fig.savefig(run_dir / f"preds_{run_id}.png")
+  plt.close(fig)
 
   # Save loss curve over time
-  plt.clf()
-  plt.figure()
-  plt.plot(history.history["loss"], label="train")
-  plt.plot(history.history["val_loss"], label="val")
-  plt.legend()
-  plt.title("Loss Curve")
-  plt.savefig(run_dir / f"loss_{run_id}.png")
+  fig, ax = plt.subplots()
+  epochs = np.arange(1, len(history.history["loss"]) + 1)
+  ax.plot(epochs, history.history["loss"], label="train")
+  ax.plot(epochs, history.history["val_loss"], label="val")
+  ax.set_title("Loss Curve")
+  ax.set_xlabel("Epoch")
+  ax.set_xticks(np.arange(1, config["epochs"]+1, 1))
+  ax.set_ylabel("Loss")
+  ax.legend()
+  fig.savefig(run_dir / f"loss_{run_id}.png")
+  plt.close(fig)
 
   # Save model
   model.save(run_dir / f"model_{run_id}.keras")
@@ -139,35 +140,48 @@ def run(config, X_train, y_train, X_test, y_test, scaler):
     "final_val_loss": history.history["val_loss"][-1]
   }
 
-@dataclass
-class Config:
-  gru_layers: List[int]
-  time_step: int = 96
-  loss: str = "mean_squared_error"
-  lr: float = 0.001
-  batch_size: int = 32
-  epochs: int = 20
-  dropout: float= 0.2
+# ------------------------------ #
 
-def main():
-  time_step = 96
-  df = pd.read_csv(PARENT_DIR / "data" / "model_data.csv")
-  X_train, y_train, X_test, y_test, scaler = load_data(df, time_step=time_step)
+os.system('cls')
+parent_dir = Path(__file__).resolve().parent
+time_step = 96
 
-  configs = [
-    Config([32], dropout=0.1, time_step=96, loss='mean_squared_error'),
-  ]
+df = pd.read_csv(parent_dir / "data" / "model_data.csv", dtype={"SCATS Number": str})
+df.columns = df.columns.str.strip()
+df["SCATS Number"] = df["SCATS Number"].str.zfill(4)
+df = df.sort_values(["Date", "SCATS Number", "hour"]).reset_index(drop=True)
+df = pd.get_dummies(df, columns=["SCATS Number"], prefix="SCATS")
 
-  results = []
+features = [
+  "hour_sin",
+  "hour_cos",
+  "day_sin",
+  "day_cos",
+  "is_weekend",
+  "lat_scaled",
+  "lon_scaled"
+] + [col for col in df.columns if col.startswith("SCATS_")]
 
-  for config in configs:
-    config = asdict(config)
-    result = run(config, X_train, y_train, X_test, y_test, scaler)
-    results.append(result)
-  
-  df = pd.DataFrame(results)
-  df.to_csv(PARENT_DIR / "runs" / "results.csv", mode="a", header=False, index=False)
+df[features] = df[features].astype(np.float32)
 
-if __name__ == '__main__':
-  os.system('cls')
-  main()
+X_train, y_train, X_test, y_test, scaler = load_data(df, features, time_step=time_step)
+
+configs = [
+  Config([16], dropout=0.2, time_step=24, epochs=20, loss='mean_squared_error'),
+  Config([32], dropout=0.1, time_step=24, epochs=20, loss='mean_squared_error'),
+  Config([32], dropout=0.2, time_step=48, epochs=20, loss='mean_squared_error'),
+  Config([32], dropout=0.2, time_step=24, epochs=30, loss='mean_squared_error'),
+  Config([32, 16], dropout=0.2, time_step=24, epochs=20, loss='mean_squared_error'),
+  Config([16, 16], dropout=0.2, time_step=24, epochs=20, loss='mean_squared_error'),
+  Config([32, 16, 8], dropout=0.2, time_step=24, epochs=20, loss='mean_squared_error'),
+]
+
+results = []
+
+for config in configs:
+  config = asdict(config)
+  result = run(config, X_train, y_train, X_test, y_test, scaler)
+  results.append(result)
+
+df = pd.DataFrame(results)
+df.to_csv(parent_dir / "runs" / "results.csv", mode="a", header=False, index=False)
